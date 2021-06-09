71ec678df48c5c3359944fa1f2e0f62af24dc03e
Puleeno Nguyen <puleeno@gmail.com>
2021-06-09 14:15:39 +0700

    Load jankx/elementor

--- bootstrap.php
+++ bootstrap.php
@@ -1,1 +1,13 @@
 <?php
+use Jankx\Elementor\Elementor;
+
+define( 'JANKX_ELEMENTOR_ROOT_DIR', dirname(__FILE__) );
+
+if (class_exists(Elementor::class))
+{
+    $elementor = new Elementor();
+    add_action(
+        'after_setup_theme',
+        array($elementor, 'integrate')
+    );
+}
--- src/Elementor.php
+++ src/Elementor.php
@@ -3,6 +3,8 @@ namespace Jankx\Elementor;
 
 use ReflectionClass;
 use Elementor\Controls_Manager;
+use Elementor\Plugin;
+use Elementor\Preview;
 
 use Jankx\Elementor\Widgets\Posts;
 use Jankx\Elementor\Widgets\PostsTabs;
@@ -10,6 +12,9 @@ use Jankx\Elementor\Widgets\PostsTabs;
 class Elementor
 {
     const POST_META_TAB = 'post_meta_tab';
+    const VERSION = '1.0.0';
+
+    protected static $assetDirUrl;
 
     public function integrate()
     {
@@ -31,6 +36,8 @@ class Elementor
         if (apply_filters('jankx_plugin_elementor_silent_mode', false)) {
             add_filter('elementor/editor/localize_settings', array( $this, 'removeElementPromtionWidgets'));
         }
+
+        add_action('wp_enqueue_scripts', array($this, 'registerScripts'));
     }
 
     public function registerJankxCategory($elementsManager)
@@ -87,5 +94,36 @@ class Elementor
 
         $widgetCategories = array_merge($highPriorityCategories, $widgetCategories);
         $widgetCategoryRefProp->setValue($elementManager, $widgetCategories);
+    }
+
+    public static function asset_url($src)
+    {
+        if (is_null(static::$assetDirUrl)) {
+            $assetDir = sprintf('%s/assets', JANKX_ELEMENTOR_ROOT_DIR);
+            $abspath = constant('ABSPATH');
+            if (PHP_OS === 'WINNT') {
+                $abspath = str_replace('\\', '/', $abspath);
+                $assetDir = str_replace('\\', '/', $assetDir);
+            }
+            static::$assetDirUrl = str_replace($abspath, site_url('/'), $assetDir);
+        }
+
+        return sprintf('%s/%s', static::$assetDirUrl, $src);
+    }
+
+    public function registerScripts()
+    {
+        $elementorPreview = Plugin::$instance->preview;
+
+        if (is_a($elementorPreview, Preview::class) && $elementorPreview->is_preview_mode()) {
+            wp_register_style(
+                'elementor-edit-mode',
+                static::asset_url('css/elementor-edit-mode.css'),
+                array(),
+                static::VERSION
+            );
+
+            wp_enqueue_style('elementor-edit-mode');
+        }
     }
 }
